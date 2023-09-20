d5a54c84bc7316b0986b710b03aa1b8a3c588a68
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-09-20 13:13:08 +0200

    collegamenti css per il timer

--- assets/script/timer.js
+++ assets/script/timer.js
@@ -6,12 +6,13 @@ function startTimer(durationInSeconds) {
 
         if (timer <= 0) {
             clearInterval(intervalId);
-            // Dopo aver fermato il timer, puoi passare alla prossima domanda qui.
+            
             
         }
+       
 
         timer--;
     }, 1000); // 1000 millisecondi (1 secondo)
 }
 
-// startTimer(70);
+startTimer(70);
