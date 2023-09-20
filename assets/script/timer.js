0b20712eded707a10b105d7620dec3a95b7bc13f
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-09-20 15:00:47 +0200

    timer funzionante nella pagina

--- assets/script/timer.js
+++ assets/script/timer.js
@@ -1,8 +1,9 @@
-function startTimer(durationInSeconds) {
+function startTimer(durationInSeconds,display) {
     let timer = durationInSeconds;
 
     const intervalId = setInterval(function () {
-        console.log(timer);
+        // console.log(timer);
+        display.textContent = timer;
 
         if (timer <= 0) {
             clearInterval(intervalId);
@@ -15,4 +16,9 @@ function startTimer(durationInSeconds) {
     }, 1000); // 1000 millisecondi (1 secondo)
 }
 
-startTimer(70);
+const timerElements = document.querySelectorAll('.tempo');
+
+timerElements.forEach(function (element) {
+    startTimer(70, element); // Imposta il tempo iniziale in secondi qui
+});
+
