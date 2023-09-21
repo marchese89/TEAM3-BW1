34f8471653859e46f327ff7824223daab91dc3ff
marchese89 <marchese89@hotmail.com>
2023-09-21 08:10:42 +0200

    aggiustamenti alle stelline

--- assets/script/feedback2.js
+++ assets/script/feedback2.js
@@ -1,39 +1,26 @@
-const Stellinespente = function () {
-  const stelline = document.querySelectorAll("#stars div img");
+const stellinespente = function () {
+  const stelline = document.querySelectorAll(".stars a img");
   stelline.forEach((element) => {
     element.classList.add("star-not-filled");
   });
 };
 
-Stellinespente();
+stellinespente();
 
-const onMouseOver = (e) => {
-  if (e === null || e === undefined || e.target.id === "") {
-    return;
-  }
-  let id = e.target.id;
-  console.log("id", id);
+const onMouseClick = (e) => {
+  let id = e.target.parentElement.id;
   for (let i = 1; i <= 10; i++) {
     let elem = document.getElementById(i).getElementsByTagName("img")[0];
     if (i <= id) {
       elem.classList.remove("star-not-filled");
+      elem.setAttribute("style", "");
     } else {
       elem.classList.add("star-not-filled");
     }
   }
 };
 
-const onMouseOut = (e) => {
-  if (e === null || e === undefined || e.target.id === "") {
-    return;
-  }
-
-  // Stellinespente();
-};
-
 for (let i = 1; i <= 10; i++) {
-  // let star = document.getElementById(i);
   let star = document.getElementById(i);
-  star.addEventListener("mouseover", onMouseOver);
-  // star.addEventListener("mouseout", onMouseOut);
+  star.addEventListener("click", onMouseClick);
 }
